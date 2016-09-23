2eeb83b83ea520f93fd38b35ab9941554ee76723
Steven Cardini <steven@cardini.ch>
2016-09-23 16:34:55 +0200

    added first version of about me description

--- index.php
+++ index.php
@@ -146,7 +146,7 @@
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <h2 class="section-heading"><?php echo I18n::t("menu.about"); ?></h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    <h3 class="section-subheading text-muted"><?php echo I18n::t("about.subtitle"); ?></h3>
                 </div>
             </div>
             <div class="row">
@@ -154,29 +154,29 @@
                     <ul class="timeline">
                         <li>
                             <div class="timeline-image">
-                                <img class="img-circle img-responsive" src="resources/img/about/1.jpg" alt="">
+                                <img class="img-circle img-responsive" src="<?php echo IMG_DIR; ?>/about/dna-163466_200.jpg" alt="">
                             </div>
                             <div class="timeline-panel">
                                 <div class="timeline-heading">
-                                    <h4>2009-2011</h4>
-                                    <h4 class="subheading">Our Humble Beginnings</h4>
+                                    <h4>2007-2010</h4>
+                                    <h4 class="subheading"><?php echo I18n::t("about.item1.title"); ?></h4>
                                 </div>
                                 <div class="timeline-body">
-                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                                    <p class="text-muted"><?php echo I18n::t("about.item1.description"); ?></p>
                                 </div>
                             </div>
                         </li>
                         <li class="timeline-inverted">
                             <div class="timeline-image">
-                                <img class="img-circle img-responsive" src="resources/img/about/2.jpg" alt="">
+                                <img class="img-circle img-responsive" src="<?php echo IMG_DIR; ?>/about/bacteria-67659_200.jpg" alt="">
                             </div>
                             <div class="timeline-panel">
                                 <div class="timeline-heading">
-                                    <h4>March 2011</h4>
-                                    <h4 class="subheading">An Agency is Born</h4>
+                                    <h4>2010-2012</h4>
+                                    <h4 class="subheading"><?php echo I18n::t("about.item2.title"); ?></h4>
                                 </div>
                                 <div class="timeline-body">
-                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                                    <p class="text-muted"><?php echo I18n::t("about.item2.description"); ?></p>
                                 </div>
                             </div>
                         </li>
@@ -186,25 +186,25 @@
                             </div>
                             <div class="timeline-panel">
                                 <div class="timeline-heading">
-                                    <h4>December 2012</h4>
-                                    <h4 class="subheading">Transition to Full Service</h4>
+                                    <h4>Since 2012</h4>
+                                    <h4 class="subheading"><?php echo I18n::t("about.item3.title"); ?></h4>
                                 </div>
                                 <div class="timeline-body">
-                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                                    <p class="text-muted"><?php echo I18n::t("about.item3.description"); ?></p>
                                 </div>
                             </div>
                         </li>
                         <li class="timeline-inverted">
                             <div class="timeline-image">
-                                <img class="img-circle img-responsive" src="resources/img/about/4.jpg" alt="">
+                                <img class="img-circle img-responsive" src="<?php echo IMG_DIR; ?>/about/matrix-356024_200.jpg" alt="">
                             </div>
                             <div class="timeline-panel">
                                 <div class="timeline-heading">
-                                    <h4>July 2014</h4>
-                                    <h4 class="subheading">Phase Two Expansion</h4>
+                                    <h4>2013-2017</h4>
+                                    <h4 class="subheading"><?php echo I18n::t("about.item4.title"); ?></h4>
                                 </div>
                                 <div class="timeline-body">
-                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                                    <p class="text-muted"><?php echo I18n::t("about.item4.description"); ?></p>
                                 </div>
                             </div>
                         </li>
